a78acfa18e4d613cacf844949ca3220bd7803604
Ramiz Ahmed <ramizahmedar@gmail.com>
2024-03-31 08:32:16 +0500

    Add schema

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+import { Form, redirect } from '@remix-run/react';
+import type { ActionFunctionArgs } from '@remix-run/node';
+
+const db = new PrismaClient();
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const { date, type, text } = Object.fromEntries(formData);
+  if (
+    typeof date !== 'string' ||
+    typeof type !== 'string' ||
+    typeof text !== 'string'
+  ) {
+    throw new Error('Bad request');
+  }
+  await db.entry.create({
+    data: {
+      date: new Date(date),
+      type,
+      text,
+    },
+  });
+  return redirect('/');
+}
+
 export default function Index() {
   return (
     <div className='p-10'>
@@ -6,7 +32,57 @@ export default function Index() {
         Learnings and doings. Updated weekly.
       </p>
 
-      <div className='mt-4'>
+      <div className='my-8 border p-2'>
+        <Form method='post'>
+          <p className='italic'>Create an entry</p>
+          <div>
+            <div className='mt-4'>
+              <input type='date' name='date' className='text-gray-700' />
+            </div>
+            <div className='mt-2 space-x-4'>
+              <label>
+                <input className='mr-1' type='radio' name='type' value='work' />
+                Work
+              </label>
+              <label>
+                <input
+                  className='mr-1'
+                  type='radio'
+                  name='type'
+                  value='learning'
+                />
+                Learning
+              </label>
+              <label>
+                <input
+                  className='mr-1'
+                  type='radio'
+                  name='type'
+                  value='interesting-thing'
+                />
+                Interesting thing
+              </label>
+            </div>
+            <div className='mt-2'>
+              <textarea
+                name='text'
+                className='w-full text-gray-700'
+                placeholder='Write your entry...'
+              />
+            </div>
+            <div className='mt-1 text-right'>
+              <button
+                className='bg-blue-500 px-4 py-1 font-medium text-white '
+                type='submit'
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </Form>
+      </div>
+
+      <div className='mt-6'>
         <p className='font-bold'>
           Week of March 30<sup>th</sup>
         </p>
